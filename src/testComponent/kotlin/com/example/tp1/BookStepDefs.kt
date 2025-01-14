046b08d64f501d924f5d321a7f846e2a91bd7938
Lapanxd <morgan.dumas@outlook.fr>
2025-01-14 16:16:59 +0100

    fix

--- src/testComponent/kotlin/com/example/tp1/BookStepDefs.kt
+++ src/testComponent/kotlin/com/example/tp1/BookStepDefs.kt
@@ -1,29 +1,53 @@
 package com.example.tp1
 
-import io.cucumber.java.en.And
+import io.cucumber.java.Before
+import io.cucumber.java.Scenario
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
-import org.assertj.core.api.Assertions.assertThat
+import io.kotest.matchers.shouldBe
+import io.restassured.RestAssured
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import io.restassured.path.json.JsonPath
+import io.restassured.response.ValidatableResponse
+import org.springframework.boot.test.web.server.LocalServerPort
 
 data class Book(val name: String, val author: String)
 
 class BookStepDefs {
+    @LocalServerPort
+    private var port: Int? = 0
 
-    private val books = mutableListOf<Book>()
+    @Before
+    fun setup(scenario: Scenario) {
+        RestAssured.baseURI = "http://localhost:$port"
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+    }
 
     @When("the user adds the book {string} authored by {string}")
-    fun theUserAddsTheBook(name: String, author: String) {
-        books.add(Book(name, author))
+    fun createBook(title: String, author: String) {
+        given()
+            .contentType(ContentType.JSON)
+            .body("""{"name":"$title","author":"$author"}""")
+            .post("/books")
+            .then()
+            .statusCode(201)
     }
 
-    @And("the user retrieves all books")
-    fun theUserRetrievesAllBooks() {
-        // Retrieval is implicit since we're directly using the `books` list
+    @When("the user gets all books")
+    fun getAllBooks() {
+        lastBookResult = given().get("/books").then().statusCode(200)
     }
 
-    @Then("the list should include the following books in the same sequence")
-    fun theListShouldIncludeTheFollowingBooks(dataTable: List<Map<String, String>>) {
-        val expectedBooks = dataTable.map { row -> Book(row["name"]!!, row["author"]!!) }
-        assertThat(books).containsExactlyElementsOf(expectedBooks)
+    @Then("the list should contain the following books in the same order")
+    fun shouldHaveListOfBooks(payload: List<Map<String, Any>>) {
+        val expectedResponse = payload.joinToString(",", "[", "]") { book ->
+            book.entries.joinToString(",", "{", "}") { """"${it.key}":"${it.value}"""" }
+        }
+        lastBookResult.extract().body().jsonPath().prettify() shouldBe JsonPath(expectedResponse).prettify()
+    }
+
+    companion object {
+        lateinit var lastBookResult: ValidatableResponse
     }
 }
